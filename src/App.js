4645199e06be230fa8855f4b24b4a15e3280cef7
HristovCodes <ivailohristov1990@gmail.com>
2020-03-27 17:07:13 +0200

    Finished

--- src/App.js
+++ src/App.js
@@ -16,22 +16,111 @@ class Tab extends React.Component {
     this.changeTabToMenu = this.changeTabToMenu.bind(this);
     this.changeTabToAbout = this.changeTabToAbout.bind(this);
     this.state = {
-      tab: Menu,
-      title: "Menu",
-      text: "Welcome to the best sushi store in town!"
+      tab: Home,
+      title: <h1>Indigo</h1>,
+      text: (
+        <h2>
+          Located in the hotel lobby, our restaurant offers a large variety of
+          Japanese inspired cuisine and is associated with perfect taste and
+          extraordinary dishes. For this restaurant with this cool name it is
+          not only the menu that offers the perfect taste, but the bar is
+          something unique, complementing the food in the best way possible. In
+          addition to the Japanese spirits and the famous sake, there is large
+          selection of top-quality wines to choose from as well as a selection
+          of Champaign and Prosecco. Designed to satisfy even the most exigent
+          tastes in matter of food and drinks, the
+          <span> Indigo Sushi Restaurant </span>also impresses with its large
+          open kitchen where the restaurant’s experienced Japanese Chefs show
+          their culinary talents in preparing the so healthy, nourishing and
+          appetizing dishes.
+        </h2>
+      )
     };
   }
 
   changeTabToHome() {
     this.setState({ tab: Home });
+    this.setState({ title: <h1>Indigo</h1> });
+    this.setState({
+      text: (
+        <h2>
+          Located in the hotel lobby, our restaurant offers a large variety of
+          Japanese inspired cuisine and is associated with perfect taste and
+          extraordinary dishes. For this restaurant with this cool name it is
+          not only the menu that offers the perfect taste, but the bar is
+          something unique, complementing the food in the best way possible. In
+          addition to the Japanese spirits and the famous sake, there is large
+          selection of top-quality wines to choose from as well as a selection
+          of Champaign and Prosecco. Designed to satisfy even the most exigent
+          tastes in matter of food and drinks, the
+          <span> Indigo Sushi Restaurant </span>also impresses with its large
+          open kitchen where the restaurant’s experienced Japanese Chefs show
+          their culinary talents in preparing the so healthy, nourishing and
+          appetizing dishes.
+        </h2>
+      )
+    });
   }
 
   changeTabToMenu() {
     this.setState({ tab: Menu });
+    this.setState({ title: <h1>Menu</h1> });
+    this.setState({
+      text: (
+        <div className="Display">
+          <div className="Card">
+            <img
+              src="https://images.unsplash.com/photo-1554502078-ef0fc409efce?ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80"
+              alt="Albacore with Crispy Onion"
+            ></img>
+            <h2>Albacore with Crispy Onion</h2>
+            <p>$6.95</p>
+          </div>
+          <div className="Card">
+            <img
+              src="https://images.unsplash.com/photo-1558985212-92c2ff0b56e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+              alt="Takoyaki"
+            ></img>
+            <h2>Takoyaki</h2>
+            <p>$5.95</p>
+          </div>
+          <div className="Card">
+            <img
+              src="https://images.unsplash.com/photo-1553701275-1d6118df60bf?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              alt="Edamame"
+            ></img>
+            <h2>Edamame</h2>
+            <p>$3.95</p>
+          </div>
+          <div className="Card">
+            <img
+              src="https://images.unsplash.com/photo-1559410545-0bdcd187e0a6?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+              alt="Shishito Pepper"
+            ></img>
+            <h2>Shishito Pepper</h2>
+            <p>$6.95</p>
+          </div>
+        </div>
+      )
+    });
   }
 
   changeTabToAbout() {
     this.setState({ tab: About });
+    this.setState({ title: <h1>It’s More Fun In Indigo!</h1> });
+    this.setState({
+      text: (
+        <h2>
+          Lively music, a vibrant crowd, inventive cocktails and the best sushi
+          this side of California. You know when you’re in <span> Indigo </span>
+          because there’s no other place like it. Order up the best sushi in
+          town and delight in our signature rolls or amazing Japanese inspired
+          cuisine. Whatever you choose, it’s all served with a big side of
+          Anything Can Happen. So bring your friends or make new ones. Either
+          way, you’re in for a great time.
+        </h2>
+      )
+    });
   }
 
   render() {
@@ -82,8 +171,8 @@ class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <h1>{this.props.title}</h1>
-        <p>{this.props.text}</p>
+        {this.props.title}
+        {this.props.text}
       </div>
     );
   }
@@ -93,8 +182,8 @@ class Menu extends React.Component {
   render() {
     return (
       <div className="Menu">
-        <h1>{this.props.title}</h1>
-        <p>{this.props.text}</p>
+        {this.props.title}
+        {this.props.text}
       </div>
     );
   }
@@ -104,8 +193,8 @@ class About extends React.Component {
   render() {
     return (
       <div className="About">
-        <h1>{this.props.title}</h1>
-        <p>{this.props.text}</p>
+        {this.props.title}
+        {this.props.text}
       </div>
     );
   }
