6770d0054684210dd41a699e89f6aad698fb77e7
HristovCodes <ivailohristov1990@gmail.com>
2020-03-28 10:35:00 +0200

    Updates

---
+++ precache-manifest.f8f02323c30ada7f30144873e526a22e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9f648ae8fe4338b3d06bb0a55d8644",
+    "url": "/restaurant-page-reactjs/index.html"
+  },
+  {
+    "revision": "0b1d044f4dafad10c703",
+    "url": "/restaurant-page-reactjs/static/css/main.7dd30cc7.chunk.css"
+  },
+  {
+    "revision": "92cafe1d7b5f949f369b",
+    "url": "/restaurant-page-reactjs/static/js/2.44c9c269.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restaurant-page-reactjs/static/js/2.44c9c269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b1d044f4dafad10c703",
+    "url": "/restaurant-page-reactjs/static/js/main.583f432d.chunk.js"
+  },
+  {
+    "revision": "3c2a5e1ac87028393bb5",
+    "url": "/restaurant-page-reactjs/static/js/runtime-main.6271529f.js"
+  }
+]);
